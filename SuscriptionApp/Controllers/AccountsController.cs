6732f578998d7a6896183e28603807eaef0944c4
Yair Rodriguez <yair.rodriguezmartinez@maritz.com>
2023-01-20 16:35:22 -0600

    Register endpoint

---
+++ SuscriptionApp/Controllers/AccountsController.cs
@@ -0,0 +1,65 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.IdentityModel.Tokens;
+using SuscriptionApp.DTOs;
+using System.IdentityModel.Tokens.Jwt;
+using System.Security.Claims;
+using System.Text;
+
+namespace SuscriptionApp.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class AccountsController : ControllerBase
+    {
+        private readonly UserManager<IdentityUser> userManager;
+        private readonly IConfiguration configuration;
+
+        public AccountsController(UserManager<IdentityUser> userManager,
+            IConfiguration configuration)
+        {
+            this.userManager = userManager;
+            this.configuration = configuration;
+        }
+        [HttpPost("register")]
+        public async Task<ActionResult<AuthenticationResponse>> Register(UserCredentials userCredentials)
+        {
+            var user = new IdentityUser { 
+                UserName = userCredentials.Email, 
+                Email = userCredentials.Email 
+            };
+            var result = await userManager.CreateAsync(user, userCredentials.Password);
+            
+            if(result.Succeeded)
+            {
+                return BuildToken(userCredentials);
+            }
+            else
+            {
+                return BadRequest(result.Errors);
+            }
+        }
+        private AuthenticationResponse BuildToken(UserCredentials userCredentials)
+        {
+            var claims = new List<Claim>
+            {
+                new Claim("email", userCredentials.Email)
+            };
+
+            var key = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(configuration["keyjwt"]));
+            var creds =  new SigningCredentials(key, SecurityAlgorithms.HmacSha256);
+
+            var expiration = DateTime.UtcNow.AddYears(1);
+
+            var securityToken = new JwtSecurityToken(issuer: null, audience: null, 
+                claims: claims, expires: expiration, signingCredentials: creds);
+
+            return new AuthenticationResponse
+            {
+                Token = new JwtSecurityTokenHandler().WriteToken(securityToken),
+                Expiration = expiration
+            };
+        }
+    }
+}
---
+++ SuscriptionApp/DTOs/AuthenticationResponse.cs
@@ -0,0 +1,10 @@
+﻿using System.Reflection.Metadata.Ecma335;
+
+namespace SuscriptionApp.DTOs
+{
+    public class AuthenticationResponse
+    {
+        public string Token { get; set; }
+        public DateTime Expiration { get; set; }
+    }
+}
