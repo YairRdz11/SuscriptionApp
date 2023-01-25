7deb96597ae6299446d5a8e0766b38b54efe2712
Yair Rodriguez <yair.rodriguezmartinez@maritz.com>
2023-01-25 17:08:20 -0600

    Adding validation to limit request middleware

--- SuscriptionApp/Middlewares/LimitRequestMiddlewareExtensions.cs
+++ SuscriptionApp/Middlewares/LimitRequestMiddlewareExtensions.cs
@@ -1,5 +1,7 @@
-﻿using SuscriptionApp;
+﻿using Microsoft.EntityFrameworkCore;
+using SuscriptionApp;
 using SuscriptionApp.DTOs;
+using SuscriptionApp.Entities;
 
 namespace SuscriptionApp.Middlewares
 {
@@ -36,6 +38,54 @@ public class LimitRequestMiddleware
             await httpContext.Response.WriteAsync("You should provide the key: X-Api-Key");
             return;
         }
+        if(keystringValue.Count > 1)
+        {
+            httpContext.Response.StatusCode = 400;
+            await httpContext.Response.WriteAsync("You should have only one key");
+            return;
+        }
+
+        var key = keystringValue[0];
+
+        var keyDB = await context.KeysAPI.FirstOrDefaultAsync(x => x.Key == key);
+
+        if (keyDB == null)
+        {
+            httpContext.Response.StatusCode = 400;
+            await httpContext.Response.WriteAsync("The key doesn't exist");
+            return;
+        }
+
+        if (!keyDB.Enable)
+        {
+            httpContext.Response.StatusCode = 400;
+            await httpContext.Response.WriteAsync("The key is disabled");
+            return;
+        }
+        if(keyDB.KeyType == SuscriptionApp.Enums.KeyType.Free)
+        {
+            var today = DateTime.Today;
+            var tomorrow = today.AddDays(1);
+
+            var countRequestToday = await context.Requests
+                .CountAsync(x => 
+                x.KeyId == keyDB.Id && x.RequestDate >= today && x.RequestDate < tomorrow);
+            if(countRequestToday >= limitRequestConfiguration.RequestsByDayForFree ) 
+            {
+                httpContext.Response.StatusCode = 429;
+                await httpContext.Response.WriteAsync("You have sent the request limit per day, if you want perform more update your subscription for a professional account");
+                return;
+            }
+        }
+
+        var request = new Request()
+        {
+            KeyId = keyDB.Id,
+            RequestDate = DateTime.UtcNow
+        };
+
+        context.Add(request);
+        await context.SaveChangesAsync();
 
         await next(httpContext);
     }
